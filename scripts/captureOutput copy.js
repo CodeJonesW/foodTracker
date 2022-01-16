cb43bcefb5282cb29a02f9b1f7ff6d3ed8dbcd46
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2022-01-16 00:13:17 -0500

    actiosn

--- scripts/captureOutput copy.js
+++ scripts/captureOutput copy.js
@@ -1,31 +1,20 @@
 const {spawn} = require('child_process');
-const core = require('@actions/core');
-const exec = require('@actions/exec');
+
 const proc = spawn('node', ['./animalLogger.js']);
 
-// let output = '';
+let output = '';
 
-// proc.stdout.on('data', chunk => {
-//   output += chunk.toString();
-// });
-// proc.on('exit', () => {
-//   var filterArray = output.split('\n').filter(function (val) {
-//     return /@considr-it\/ponder-server+:\sStatements/.test(val);
-//   });
+proc.stdout.on('data', chunk => {
+  output += chunk.toString();
+});
+proc.on('exit', () => {
+  var filterArray = output.split('\n').filter(function (val) {
+    return /@considr-it\/ponder-server+:\sStatements/.test(val);
+  });
 
-//   console.log(filterArray);
-// });
+  console.log(filterArray);
+});
 
-// proc.stderr.on('data', data => {
-//   console.error(`child stderr:\n${data}`);
-// });
-
-async function run() {
-  try {
-    const someInput = core.getInput('some_input');
-    await exec.exec(`bash ${__dirname}/some-bash-script.sh ${someInput}`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-}
-run();
+proc.stderr.on('data', data => {
+  console.error(`child stderr:\n${data}`);
+});
