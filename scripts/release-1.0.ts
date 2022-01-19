2d2abc9b31da07dda8e55f56062076cf0430bddf
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2022-01-18 22:41:17 -0500

    feature3

---
+++ scripts/release-1.0.ts
@@ -0,0 +1,9 @@
+// new feature for release 1.0.0
+
+export const newFeature2 = () => {
+  console.log('What an awesome feature'!);
+};
+
+export const newFeature3 = () => {
+  console.log('this is revolutionary');
+};
