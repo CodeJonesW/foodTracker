92f0fb2372d7a2353d101f8c1f1bb067d31becf8
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2022-05-27 14:00:03 -0500

    Release 1.0.1 (#12)
    * YAML | testing delayed actions |

---
+++ scripts/release-1.0.ts
@@ -0,0 +1,13 @@
+// new feature for release 1.0.0
+
+export const newFeature2 = () => {
+  console.log('What an awesome feature'!);
+};
+
+export const newFeature6 = () => {
+  console.log('What an awesome feature'!);
+};
+
+export const newFeature5 = () => {
+  console.log('this is revolutionary');
+};
